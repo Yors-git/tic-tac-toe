efd20c2fa3ae49da8d646951d0c52d1248615bc9
Yors <jatbribiesca@gmail.com>
2020-10-13 08:17:27 -0500

    Revert "Merge branch 'development' into set-up"
    This reverts commit cb8cfc5074d4065ee998775b2c1ce9864bc28d2d, reversing
    changes made to e85a210e70854232c4a6861ef1eac6094d9b015a.

--- bin/main.rb
+++ bin/main.rb
@@ -1,25 +1,2 @@
 #!/usr/bin/env ruby
-puts "Player 1 please enter your name"
-player1 = gets.chomp
-puts "Player 2 please enter your name"
-player2 = gets.chomp
-
-board = [1,2,3,4,5,6,7,8,9]
-
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts "-----------"
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
-puts "-----------"
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
-
-puts "Player 1 select the number (1-9) for your move"
-p1_move = gets.chomp.to_i
-board[p1_move-1] = "O"
-puts "Player 2 select the number (1-9) for your move"
-p2_move = gets.chomp.to_i
-board[p2_move-1] = "X"
-
-
-# puts "#{winner} you won!"
-puts "Play again? press \"Y\" or press any other key to quit"
-input = gets.chomp.upcase
+puts 'Hello world!'
