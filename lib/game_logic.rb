e6fe4a14bb29ce62d5605f17a71bb76f362bfc64
Ceejayski <61045640+Ceejayski@users.noreply.github.com>
2020-10-14 14:08:41 +0100

    Create winning_cases method and import to the main

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,19 +1,42 @@
-module GameLogic 
-    def display_board(board)
-        puts " #{board[0]} | #{board[1]} | #{board[2]} "
-        puts "-----------"
-        puts " #{board[3]} | #{board[4]} | #{board[5]} "
-        puts "-----------"
-        puts " #{board[6]} | #{board[7]} | #{board[8]} "
-    end
+# frozen_string_literal: true
 
-    def valid_input(input, arr)
-        if (input == nil || input.is_a?(Integer) == false || (1..9).include?(input)== false) || (arr[input-1] == "X" || arr[input-1] == "O")
-            false
-        end
-    end
-    
-    def winning_cases
+module GameLogic
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts '-----------'
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts '-----------'
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
 
+  def valid_input(input, arr)
+    if (input.nil? || input.is_a?(Integer) == false || (1..9).include?(input) == false) || (arr[input - 1] == 'X' || arr[input - 1] == 'O')
+      false
     end
+  end
+
+  def winning_cases(arr)
+    cases = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+
+    board_case = ' '
+    cases.each do |pattern|
+      pattern.each do |i|
+        board_case += arr[i].to_s
+      end
+    end
+    if board_case.include? 'XXX'
+      'X'
+    elsif board_case.include? 'OOO'
+      'O'
+    end
+  end
 end
--- lib/players.rb
+++ lib/players.rb
@@ -1,12 +1,16 @@
+# frozen_string_literal: true
+
 class Player
   attr_accessor :name, :mark
   def initialize(name)
-      @name = name
+    @name = name
   end
+
   def player1_mark
-      @mark = "O"
+    @mark = 'O'
   end
+
   def player2_mark
-      @mark = "X"
+    @mark = 'X'
   end
 end
