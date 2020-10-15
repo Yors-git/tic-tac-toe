65b6df71f9a5313c2c1395df5c08d92bab4b5eee
Yors <jatbribiesca@gmail.com>
2020-10-15 03:34:00 -0500

    change false to negation, remove puts outside main, append question mark to methods

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -11,15 +11,15 @@ class GameLogic
   ].freeze
 
   def self.display_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    puts '-----------'
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    puts '-----------'
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    [" #{board[0]} | #{board[1]} | #{board[2]} ",
+    "-----------",
+    " #{board[3]} | #{board[4]} | #{board[5]} ",
+    "-----------",
+    " #{board[6]} | #{board[7]} | #{board[8]} "]
   end
 
-  def self.valid_input(input, arr)
-    false if (input.nil? || input.is_a?(Integer) == false || (1..9).include?(input) == false) || (arr[input - 1] == 'X' || arr[input - 1] == 'O')
+  def self.valid_input?(input, arr)
+    false if (input.nil? || !input.is_a?(Integer) || !(1..9).include?(input)) || (arr[input - 1] == 'X' || arr[input - 1] == 'O')
   end
 
   def self.winning_cases(arr)
