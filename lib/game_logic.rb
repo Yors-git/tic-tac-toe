df979fe246f8a5f319ece34376bafcbdfd46bd3b
Ceejayski <61045640+Ceejayski@users.noreply.github.com>
2020-10-15 07:54:52 +0100

    Solve linter errors

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -2,16 +2,16 @@
 # frozen_string_literal: true
 
 module GameLogic
-    CASES = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ].freeze
+  CASES = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ].freeze
 
   def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -30,17 +30,17 @@ module GameLogic
   def winning_cases(arr)
     win = nil
     CASES.each do |pattern|
-        board_case = ""
+      board_case = ''
       pattern.each do |i|
         board_case += arr[i].to_s
       end
-      if board_case =='XXX'
+      if board_case == 'XXX'
         win = 'X'
       elsif board_case == 'OOO'
         win = 'O'
       end
     end
-    return win
+    win
   end
 end
 # rubocop: enable Style/Documentation, Style/GuardClause, Layout/LineLength, Metrics/MethodLength
