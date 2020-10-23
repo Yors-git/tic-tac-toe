1f9e9ade6aaf9d4771be4634823c4ea53fd3c8e9
Yors <jatbribiesca@gmail.com>
2020-10-23 02:38:45 -0500

    change player class to use variables for new objects

--- spec/tic_tac_toe_players_spec.rb
+++ spec/tic_tac_toe_players_spec.rb
@@ -3,31 +3,29 @@ require_relative '../lib/players'
 describe Player do
   let(:name) { 'John' }
   let(:short_name) { 'Jo' }
+  let(:player) { Player.new(name) }
 
   describe '#initialize' do
     it 'checks if the correct name is passed to @name' do
-      player = Player.new(name)
       expect(player.name).to eql('John')
     end
   end
 
   describe '#player1_mark' do
     it 'checks that "O" is asigned to mark' do
-      player = Player.new(name)
       expect(player.player1_mark).to eql('O')
     end
   end
 
   describe '#player2_mark' do
     it 'checks that "X" is asigned to mark' do
-      player = Player.new(name)
       expect(player.player2_mark).to eql('X')
     end
   end
 
   describe '#valid_name?' do
     it 'checks that the input is valid' do
-      player = Player.new(short_name)
+      player.name = short_name
       expect(player.valid_name?).to be(false)
     end
   end
