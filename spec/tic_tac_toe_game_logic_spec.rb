1dd236f774f371786fcf81a8ff3199d8a703d0c3
Yors <jatbribiesca@gmail.com>
2020-10-23 09:56:57 -0500

    Remove variables in each test to use only one

--- spec/tic_tac_toe_game_logic_spec.rb
+++ spec/tic_tac_toe_game_logic_spec.rb
@@ -1,60 +1,54 @@
 require_relative '../lib/game_logic'
 
 describe GameLogic do
+  let(:test_game) { GameLogic.new }
   describe '#display_board' do
     it 'displays the board for the game with numbers (1-9) in each cell' do
-      board = GameLogic.new
-      expect(board.display_board).to eql([' 1 | 2 | 3 ',
-                                          '-----------',
-                                          ' 4 | 5 | 6 ',
-                                          '-----------',
-                                          ' 7 | 8 | 9 '])
+      expect(test_game.display_board).to eql([' 1 | 2 | 3 ',
+                                              '-----------',
+                                              ' 4 | 5 | 6 ',
+                                              '-----------',
+                                              ' 7 | 8 | 9 '])
     end
   end
 
   describe '#valid_input?' do
     it 'returns false if the input is nil' do
-      val_inp = GameLogic.new
-      expect(val_inp.valid_input?(nil)).to eql(false)
+      expect(test_game.valid_input?(nil)).to eql(false)
     end
 
     it 'checks if the input is an integer' do
-      val_inp = GameLogic.new
-      expect(val_inp.valid_input?(4)).not_to eql(true)
+      expect(test_game.valid_input?(4)).not_to eql(true)
     end
 
     it 'checks if the input is between 1 and 9' do
-      val_inp = GameLogic.new
-      expect(val_inp.valid_input?(11)).to eql(false)
+      expect(test_game.valid_input?(11)).to eql(false)
     end
   end
 
   describe '#winning_cases' do
     it 'checks the case of XXX' do
-      win_case = GameLogic.new
-      win_case.board[3] = 'X'
-      win_case.board[4] = 'X'
-      win_case.board[5] = 'X'
-      win_case.display_board
-      win_case.winning_cases
-      expect(win_case.winning_cases).to eql('X')
+      test_game.board[3] = 'X'
+      test_game.board[4] = 'X'
+      test_game.board[5] = 'X'
+      test_game.display_board
+      test_game.winning_cases
+      expect(test_game.winning_cases).to eql('X')
     end
 
     it 'checks the case of not winning move' do
-      win_case = GameLogic.new
-      win_case.board[3] = 'O'
-      win_case.board[4] = 'X'
-      win_case.board[5] = 'X'
-      win_case.display_board
-      win_case.winning_cases
-      expect(win_case.winning_cases).to eql(nil)
+      test_game.board[3] = 'O'
+      test_game.board[4] = 'X'
+      test_game.board[5] = 'X'
+      test_game.display_board
+      test_game.winning_cases
+      expect(test_game.winning_cases).to eql(nil)
     end
   end
 
   describe '#tie_case' do
     it 'checks the case of a tie' do
-      tie_case = GameLogic.new
-      expect(tie_case.tie_case(9)).to eql(true)
+      expect(test_game.tie_case(9)).to eql(true)
     end
   end
 end
